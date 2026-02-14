bc8a0831bb95d014b645be3116a1afdb43a4d9c1
RestingBeast <mmmike2002@gmail.com>
2026-02-14 16:56:24 +0800

    Revert "Norm Fixed"
    This reverts commit 1e28c7269f49ed1b94614add151bb51b3167ec8b.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kkhant-z <kkhant-z@student.42singapore.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 15:35:41 by kkhant-z          #+#    #+#             */
-/*   Updated: 2026/02/14 16:48:53 by kkhant-z         ###   ########.fr       */
+/*   Updated: 2026/02/13 08:31:22 by kkhant-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,12 @@ static char	*trim_delimiter(char *str, char delimiter)
 	return (free(str), res);
 }
 
-static char	*extract_and_trim(char **leftover, char delimiter)
-{
-	char	*res;
-
-	res = ft_extract(*leftover, delimiter);
-	*leftover = trim_delimiter(*leftover, delimiter);
-	return (res);
-}
-
 char	*get_next_line(int fd)
 {
 	static char	*leftover = NULL;
 	const char	delimiter = '\n';
 	char		*buf;
+	char		*res;
 	int			bytes_read;
 
 	buf = malloc(BUFFER_SIZE + 1);
@@ -59,10 +51,18 @@ char	*get_next_line(int fd)
 		buf[bytes_read] = '\0';
 		leftover = ft_strjoin(leftover, buf);
 		if (ft_strchr(leftover, delimiter))
-			return (free(buf), extract_and_trim(&leftover, delimiter));
+		{
+			res = ft_extract(leftover, delimiter);
+			leftover = trim_delimiter(leftover, delimiter);
+			return (free(buf), res);
+		}
 		bytes_read = read(fd, buf, BUFFER_SIZE);
 	}
 	if (bytes_read == 0 && leftover != NULL)
-		return (free(buf), extract_and_trim(&leftover, delimiter));
+	{
+		res = ft_extract(leftover, delimiter);
+		leftover = trim_delimiter(leftover, delimiter);
+		return (free(buf), res);
+	}
 	return (free(buf), NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkhant-z <kkhant-z@student.42singapore.    +#+  +:+       +#+        */
+/*   By: kkhant-z <kkhant-z@student.42singapor      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 15:31:24 by kkhant-z          #+#    #+#             */
-/*   Updated: 2026/02/14 16:34:00 by kkhant-z         ###   ########.fr       */
+/*   Updated: 2026/02/11 15:31:27 by kkhant-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@
 char	*get_next_line(int fd);
 
 // utils
-int		ft_strchr(char *str, int c);
-int		ft_strlen(char *str);
+int	    ft_strchr(char *str, int c);
+int	    ft_strlen(char *str);
 void	ft_strcat(char *dest, char *src);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_extract(char *str, char delimiter);
